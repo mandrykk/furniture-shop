4e584ebdefa5d2ef069a8400d8c720e62a9143c7
mandrykk <mandrykk22@gmail.com>
2023-11-09 09:55:47 +0200

    adaptive design

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Функция для обновления счетчика
 function updateCountdown() {
-    const countdownDate = new Date("2026-12-31 00:00:00").getTime(); // Установите дату окончания счетчика
+    const countdownDate = new Date("2026-12-31 00:00:00").getTime();
     const currentDate = new Date().getTime();
     const timeDifference = countdownDate - currentDate;
 
@@ -21,3 +21,40 @@ setInterval(updateCountdown, 1000);
 
 // Инициализируем счетчик при загрузке страницы
 updateCountdown();
+
+// Функция для свайпера
+document.addEventListener("DOMContentLoaded", function () {
+    const sellerGrid = document.getElementById("sellerGrid");
+    const sellerCards = sellerGrid.querySelectorAll(".seller__card");
+    const prevButton = document.querySelector(".seller__nav .prev");
+    const nextButton = document.querySelector(".seller__nav .next");
+
+    let currentIndex = 0;
+    const cardsPerPage = 4;
+
+    function showCards() {
+        for (let i = 0; i < sellerCards.length; i++) {
+            if (i >= currentIndex && i < currentIndex + cardsPerPage) {
+                sellerCards[i].classList.add("visible");
+            } else {
+                sellerCards[i].classList.remove("visible");
+            }
+        }
+    }
+
+    prevButton.addEventListener("click", function () {
+        if (currentIndex > 0) {
+            currentIndex -= cardsPerPage;
+            showCards();
+        }
+    });
+
+    nextButton.addEventListener("click", function () {
+        if (currentIndex + cardsPerPage < sellerCards.length) {
+            currentIndex += cardsPerPage;
+            showCards();
+        }
+    });
+
+    showCards();
+});
